70bc381e0a47c05f97384e76486b51076207ff57
abhisheksaurabh1985 <abhishek_saurabh001@yahoo.co.in>
2015-12-30 16:05:01 +0100

    Data reading part done

--- externalFunctions.py
+++ externalFunctions.py
@@ -19,33 +19,37 @@ def generateWordMatrix(fileWordVector, fileVocab):
         return indexVocabulary, numWords, wordMatrix
         
 
-def _split_into_sentences(trainingDataFileName):
-    """
-    Extract the labels and turn the words into sentences
+def getSentences(trainingDataFileName):
 
-    :return:
-    """
-    EOL_CHARS = ".!?"
+    # Define end-of line characters
+    endOfLineCharacters = '.?!'
     allSentences = []
-    labels = []
-    with open(trainingDataFileName, 'r') as f:
-        sentence = []
-        for line in f:
-            try:
-                print line
-                word, label = line.split()
-            except:
-                continue
-            #no the end of the sentence
-            sentence += [word]
-            intLabel = 1 if label == "PERSON" else 0
-            labels += [intLabel]
-            if word in EOL_CHARS:
-                allSentences += [sentence]
-                sentence = []
+    allLabels = []
 
-        #in case the last sentence doesn't end with proper punctuation!
-        if sentence != []:
-            allSentences += [sentence]
-##        self.allSentences = allSentences
-        return allSentences, labels    
+    with open(trainingDataFileName, 'r') as fileObj:
+        sentence = []
+        for line in fileObj:
+            if len(line.split())== 2:
+                try:
+                    word, label = line.split()
+                except:
+                    continue
+                sentence = sentence + [word]    
+
+                # Training data has labels 'PERSON' and 'O' for name and non-name word respectively. Codify it as 1 and 0.
+                if label == 'PERSON':
+                    newLabel = 1
+                else:
+                    newLabel = 0
+                allLabels = allLabels + [newLabel]
+
+                if word in endOfLineCharacters:
+                    allSentences = allSentences + [sentence]
+                    sentence = []
+
+        # Last sentence does not end with any of the end of line characters defined earlier. Add it to the list of sentences.
+        if sentence!= []:
+            allSentences = allSentences + [sentence]
+
+        return allSentences, allLabels, sentence    
+
--- run_program.py
+++ run_program.py
@@ -9,7 +9,7 @@ import numpy as np
 dataDirectory = "data/"
 trainingDataFile = dataDirectory + "train"
 wordVectorFile = dataDirectory + "wordVectors.txt"
-vocabularyFile = dataDirectory +"vocab.txt"
+vocabularyFile = dataDirectory + "vocab.txt"
 
 # Dimension of each word vector 1*50
 wordVectorDimension = 50
@@ -18,5 +18,8 @@ wordVectorDimension = 50
 # Each column of the word matrix corresponds to the vector representation of a word in the vocabulary. 
 indexedVocabulary, vocabWordCount, wordMatrix = generateWordMatrix(wordVectorFile, vocabularyFile)
 
+all_sentences, all_labels, sentence = getSentences(trainingDataFile)
+print sentence
 
-labels, sentences = _split_into_sentences(trainingDataFile)
+##sentences, labels, sentence = _split_into_sentences(trainingDataFile)
+##print sentence
