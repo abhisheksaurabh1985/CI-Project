36342bf5190816e3b5a6d82a2883e70c04d41aa9
abhisheksaurabh1985 <abhishek_saurabh001@yahoo.co.in>
2016-01-15 15:23:23 +0100

    Matrix dimensions printed

--- run_program.py
+++ run_program.py
@@ -1,4 +1,5 @@
 execfile('./externalFunctions.py')
+execfile('./NeuralNetwork.py')
 
 from collections import OrderedDict
 
@@ -17,9 +18,15 @@ wordVectorDimension = 50
 # Get index of the words in the vocabulary, count of words in the vocabulary and the word matrix from the vocabulary.
 # Each column of the word matrix corresponds to the vector representation of a word in the vocabulary. 
 indexedVocabulary, vocabWordCount, vocabWordMatrix = generateWordMatrix(wordVectorFile, vocabularyFile)
+print 'Total number of words in the vocabulary: ', vocabWordCount
+print '\nProperties of vocabulary word matrix are as under:'
+print 'Data type of container: ', type(vocabWordMatrix)
+print 'Number of dimensions (aka rank): ', vocabWordMatrix.ndim
+print 'Dimensions of the array (row*col): ', vocabWordMatrix.shape
+print 'Total number of elements: ', vocabWordMatrix.size
 
 all_sentences, all_labels, lastSentence = getSentences(trainingDataFile)
-print lastSentence
+print '\nNumber of sentences in the training data: ', len(all_sentences)
 
 windowPadding = 1
 ## contextSize = 3
@@ -27,3 +34,26 @@ nestedListSentenceTuples, listSentenceTuples = generateWordWindows(windowPadding
 
 # Generate word vector
 training_matrix, labelsAsMatrix = generateWordVectors(wordVectorDimension, vocabWordMatrix, all_labels, windowPadding, listSentenceTuples)
+print '\nProperties of training data matrix are as under:'
+print 'Data type of container: ', type(training_matrix)
+print 'Number of dimensions (aka rank): ', training_matrix.ndim
+print 'Dimensions of the array (row*col): ', training_matrix.shape
+print 'Total number of elements: ', training_matrix.size
+
+print '\nProperties of training data labels matrix are as under:'
+print 'Data type of container: ', type(labelsAsMatrix)
+print 'Number of dimensions (aka rank): ', labelsAsMatrix.ndim
+print 'Dimensions of the array (row*col): ', labelsAsMatrix.shape
+print 'Total number of elements: ', labelsAsMatrix.size
+
+# Test feed forward
+##from NeuralNetwork import NeuralNetwork
+##a= NeuralNetwork(len(listSentenceTuples), 100, 2)
+##a.feedForwardNetwork(listSentenceTuples)  
+
+##trainingData = 
+##numberOfEpochs = 30
+##for j in xrange(numberOfEpochs):
+##    random.shuffle(trainingData)
+##
+
